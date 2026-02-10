b956a6490b6e136c854d54e0b3bf74ab0fe0914b
YOUR_NAME <aaryaatugaonkar007@gmail.com>
2026-02-11 02:23:42 +0530

    update

--- C Programming/Assignments/Assignment 3/Q7.c
+++ C Programming/Assignments/Assignment 3/Q7.c
@@ -1,11 +1,11 @@
 void main (){
-    int n=5,i=n,op=1;
+    int n=5,i=n,o=1;
     while(i!=0&&i<=n){
         if(n!=0){ 
-            op=op*n;
+            o=o*n;
         }
         n--;
         i--;  
     }
-    printf("%d",op);
+    printf("%d",o);
 }
