ab20271c2d81aa5635146c8aec2f39c40d4a4713
Aaryaa Tugaonkar <aaryaatugaonkar007@gmail.com>
2026-02-11 19:26:00 +0530

    Q7.c

--- C Programming/Assignments/Assignment 3/Q7.c
+++ C Programming/Assignments/Assignment 3/Q7.c
@@ -1,11 +1,9 @@
 void main (){
     int n=5,i=n,o=1;
-    while(i!=0&&i<=n){
-        if(n!=0){ 
+    while(n!=0){
             o=o*n;
-        }
-        n--;
-        i--;  
+        
+        n--;  
     }
     printf("%d",o);
 }
