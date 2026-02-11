d80cdc299c06b5a0b4ea221804bd10af59239520
Aaryaa Tugaonkar <aaryaatugaonkar007@gmail.com>
2026-02-11 22:02:31 +0530

    Update Q4.c

--- C Programming/Assignments/Assignment 3/Q4.c
+++ C Programming/Assignments/Assignment 3/Q4.c
@@ -1,14 +1,11 @@
 void main() {
     int a = 7;
     int i = 2;
-    int isPrime = 1;
-    if (a <= 1) {
-        isPrime = 0;
-    }
+    int isPrime = 0;
     while (i < a) {
         if (a % i == 0) {
             isPrime = 0;
-            
+            break;
         }
         i++;
     }
