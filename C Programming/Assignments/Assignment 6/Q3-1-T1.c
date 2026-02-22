7d5ec3bf93613632550b57f60c33561753048f09
YOUR_NAME <aaryaatugaonkar007@gmail.com>
2026-02-22 18:09:44 +0530

    added remaining files

---
+++ C Programming/Assignments/Assignment 6/Q3-1-T1.c
@@ -0,0 +1,10 @@
+void print();
+void main()
+{
+print();
+}
+void print()
+{
+     for (int i;i<=10;i++)
+    printf("%d\n",i);
+}
--- C Programming/Lab/addition of two no.cpp
+++
@@ -1,8 +0,0 @@
-void main ()
-{
-
-	int a=50;
-	int b=60;
-	int add=a+b;
-	printf("addition of two nos is%d",add);
-}
