e9f570bf7e289664397b03cc5b1bdeb34fdf7c20
Aaryaa Tugaonkar <aaryaatugaonkar007@gmail.com>
2026-04-01 15:19:51 +0530

    update

--- C Programming/Project A/PlayerManagement.c
+++ C Programming/Project A/PlayerManagement.c
@@ -12,26 +12,34 @@
 
 
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 
 typedef struct player {
 
     int Jno;
-    int count;
+    
     char Pname[100];
     int runs;
     int wickets;
     int mplayed;
 }player;
 
+void addplayer(player *set);
+void removeplayer(player set[], int *count);
+void searchplayer(player set[], int count);
+void updateplayer(player set[], int count);
+void sortplayer(player set[], int count);
+void display(player set[], int count);
 
 
 void main()
 {
     
     player *set=(player*)malloc(sizeof(player)*11);
-    int in;
-    set->count=0;
+    int in=-1;
+    int playercount=0;
+    int count=0;
 
     while(in!=0)
     {
@@ -41,34 +49,42 @@ void main()
         printf("\n4. Update Player");
         printf("\n5. Sort Player");
         printf("\n6. Display Player");
+        printf("\n0. To Exit");
+        scanf("%d",&in);
 
-        scanf("%d\n",&in);
-
-        if(in==1)
-        {
-            addplayer(set);
-            set->count++;
-        }
-
-        if(in==2)
-        {
-            removeplayer(set)
-            set->count--;
-        }
-
-        if(in==3)
-        {
-            searchplayer(set);
-
-        }
-
-        if(in==4)
-        {
-            updateplayer(set);
-        }
-
-
-
+        if(in==0)
+            {
+                printf("Exiting....");
+            }
+else if(in==1)
+{
+    addplayer(&set[count]);
+    count++;
+}
+else if(in==2)
+{
+    removeplayer(set, &count);
+}
+else if(in==3)
+{
+    searchplayer(set, count);
+}
+else if(in==4)
+{
+    updateplayer(set, count);
+}
+else if(in==5)
+{
+    sortplayer(set, count);
+}
+else if(in==6)
+{
+    display(set, count);
+}
+else
+{
+    printf("Wrong input");
+}
     }
     
 }
@@ -76,20 +92,45 @@ void addplayer(player *set)
 {
    
     printf("Enter the Name of Player :");
-    scanf("%s\n",set->Pname);
+    scanf("%s",set->Pname);
 
     printf("Enter the Jercey NO :");
-    scanf("%d\n",set->Jno);
+    scanf("%d",&set->Jno);
 
     printf("Enter Player Runs :");
-    scanf("%d\n",set->runs);
+    scanf("%d",&set->runs);
 
     printf("Enter Player Wickets :");
-    scanf("%d\n",set->wickets);
+    scanf("%d",&set->wickets);
 
     printf("Enter Player Matches Played :");
-    scanf("%d\n",set->mplayed);
+    scanf("%d",&set->mplayed);
 
-    
+    printf("Player Added Successfully !!");
 
+}
+
+void removeplayer(player set[], int *count)
+{
+    int rp;
+    printf("Enter the Jersey NO of Player to remove: ");
+    scanf("%d", &rp);
+
+    for(int i = 0; i < *count; i++)
+    {
+        if(set[i].Jno == rp)
+        {
+            // shift elements left
+            for(int j = i; j < *count - 1; j++)
+            {
+                set[j] = set[j + 1];
+            }
+
+            (*count)--;
+            printf("Player removed successfully!\n");
+            return;
+        }
+    }
+
+    printf("Player not found!\n");
 }
