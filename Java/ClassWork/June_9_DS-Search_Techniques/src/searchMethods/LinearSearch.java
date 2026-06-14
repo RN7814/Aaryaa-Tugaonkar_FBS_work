21a8f52b092d3d69ec30a824c987c802c2726bf8
Aaryaa Tugaonkar <aaryaatugaonkar007@gmail.com>
2026-06-14 21:21:58 +0530

    June 9 Search Techniques update

---
+++ Java/ClassWork/June_9_DS-Search_Techniques/src/searchMethods/LinearSearch.java
@@ -0,0 +1,30 @@
+package searchMethods;
+
+public class LinearSearch {
+
+	void Linear(int [] arr , int key)
+	{
+		boolean found= false;
+		for(int i=0;i<arr.length ; i++)
+		{
+			if(key == arr[i])
+				{
+					System.out.println("=====Element Found====");
+					found=true;
+					break;
+				}
+		}if ( found ==  false)
+		{
+			System.out.println("Data Not Found");
+		}
+	}
+}
+
+class Test1
+{
+	public static void main(String[] args) {
+		int[] arr = {10,20,50,4,071,95};
+		LinearSearch l1 = new LinearSearch();
+		l1.Linear(arr, 20);
+	}
+}
